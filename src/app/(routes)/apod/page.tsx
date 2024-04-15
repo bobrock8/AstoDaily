1664c903ec310bb32955ff8533b3bf2882945316
bobrock8 <aleksandarilic990@gmail.com>
2024-04-15 21:20:49 +0200

    make a logo, define font, and basic layout structure

---
+++ src/app/(routes)/apod/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>apod</div>;
+};
+
+export default page;
---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Space_Grotesk, Teko } from "next/font/google";
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const teko = Teko({
+  subsets: ["latin"],
+  display: "swap",
+});
