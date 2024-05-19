e7167adb7241b7c622eda56cfb35bab2dc51b9a1
bobrock8 <aleksandarilic990@gmail.com>
2024-05-19 12:18:20 +0200

    fix cache isse and change cron time

--- src/app/api/fetchApod.ts
+++ src/app/api/fetchApod.ts
@@ -1,10 +1,12 @@
-const APOD_URL = `https://api.nasa.gov/planetary/apod?api_key=`;
+import { getDate } from "../db/helper";
+
+const APOD_URL = `https://api.nasa.gov/planetary/apod`;
 const getApodUrl = () => {
   const apiKey = process.env.NASA_API_KEY;
   if (!apiKey) {
     throw new Error("Apod api key is missing from .env.local");
   }
-  return APOD_URL + apiKey;
+  return APOD_URL + "?date=" + getDate() + "&api_key=" + apiKey;
 };
 
 const fetchApod = async () => {
@@ -14,11 +16,13 @@ const fetchApod = async () => {
         "Cache-Control": "no-cache",
       },
     });
+
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
 
     const data = await response.json();
+    console.log("data", data);
     return data;
   } catch (error) {
     console.error(error);
