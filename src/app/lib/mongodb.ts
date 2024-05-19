2c7e064569e38c16b0ebca9fa3870c29d134a639
bobrock8 <aleksandarilic990@gmail.com>
2024-05-19 12:22:33 +0200

    fix mongo db connection for production

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -12,7 +12,10 @@ const options = {};
 let client;
 let dbClient: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "production"
+) {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
