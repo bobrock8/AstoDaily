c1175d75e0f6170c3951fb1de6814a493304be0c
bobrock8 <aleksandarilic990@gmail.com>
2024-05-31 10:54:44 +0200

    wrap route with try catch

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,11 @@
 import { saveApod } from "../saveApod";
 
 export async function GET(request: Request) {
-  saveApod();
-  return new Response(`Hello from cron`);
+  try {
+    saveApod();
+    return new Response(`Hello from cron`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
